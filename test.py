3c98ea590e531ba378c2330ae0b51bb24a4c5967
西西嘛呦 <461600371@qq.com>
2023-05-26 17:01:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -50,6 +50,7 @@ with torch.no_grad():
         preds = torch.argmax(logits, -1).detach().cpu().numpy()
         preds = np.where(labels != -100, preds, tokenizer.pad_token_id)
         preds = preds[:, :-1]
+        labels = labels[:, 1:]
         decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
         decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
